0f335fe6f2ad706c96bb1856323260f7eb2165c7
Vivek Tondare <vivek.tondare@vginfo.net>
2023-01-10 12:55:28 +0530

    tailwind css done

---
+++ 7_tailwind_css/tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  future: {},
+  purge: [],
+  theme: {
+    extend: {
+      // colors: {
+      //   indigo: {
+      //     100: "#5c6ac4",
+      //     200: "#5c6ac4",
+      //   },
+      //   blue: "#007ace",
+      //   red: "#de3618",
+      // },
+
+      // spacing: {
+      //   100: "20rem",
+    
+      // },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
